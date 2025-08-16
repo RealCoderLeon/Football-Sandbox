99173afec1b80c34feae2e301f9d11f161d9324c
RealCoderLeon <thespaceleon@gmail.com>
2025-08-16 15:41:30 +0500

    Update myLibrary.cpp

--- FSRS/myLibrary.cpp
+++ FSRS/myLibrary.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Account {
+private:
+    string username;
+    string password;
+    string team;
+    string league;
+public:
+    Account (string enteredUsername, string enteredPassword)
+        : username{enteredUsername}, password{enteredPassword} {}
+
+    string getUsername () const {
+        return username;
+    }
+    string getPassword () const {
+        return password;
+    }
+    void setTeamName (string enteredTeamName){
+        team = enteredTeamName;
+    }
+
+    string getTeam () const {
+        return team;
+    }
+};
